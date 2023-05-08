e20196bf7ebfbd7b098475ea9537f3fd9692a913
Keno <Clearwood@users.noreply.github.com>
2023-05-08 12:31:44 -0700

    removed oz

--- contracts/lzApp/LzApp.sol
+++ contracts/lzApp/LzApp.sol
@@ -34,7 +34,7 @@ abstract contract LzApp is BoringOwnable, ILayerZeroReceiver, ILayerZeroUserAppl
 
     function lzReceive(uint16 _srcChainId, bytes calldata _srcAddress, uint64 _nonce, bytes calldata _payload) public virtual override {
         // lzReceive must be called by the endpoint for security
-        require(_msgSender() == address(lzEndpoint), "LzApp: invalid endpoint caller");
+        require(msg.sender == address(lzEndpoint), "LzApp: invalid endpoint caller");
 
         bytes memory trustedRemote = trustedRemoteLookup[_srcChainId];
         // if will still block the message pathway from (srcChainId, srcAddress). should not receive message from untrusted remote.
